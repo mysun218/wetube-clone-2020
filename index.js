d9a9c707861f51c2015d1bd6f5ff0eaffe2e79db
mysun218 <mysun218@gmail.com>
2020-08-24 15:24:42 +0900

    install babel env and nodemon

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("1123");
+import express from "express";
+const app = express();
+
+const PORT = 4000;
+
+function handleListening() {
+  console.log(`Listening on : http://localhost:${PORT}`);
+}
+
+function handleHome(req, res) {
+  console.log(req);
+  res.send("hello");
+}
+
+const por = (req, res) => res.send("agrnj na fds");
+
+app.get("/", handleHome);
+app.get("/por/rrr", por);
+app.listen(PORT, handleListening);
